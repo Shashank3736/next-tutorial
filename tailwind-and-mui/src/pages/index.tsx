0068c81751095ec0c71e0e24b3c7a963d5173474
Shreyash Raj <shreyashr267@gmail.com>
2023-01-20 21:47:20 +0530

    improved button

--- tailwind-and-mui/src/pages/index.tsx
+++ tailwind-and-mui/src/pages/index.tsx
@@ -10,9 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='flex flex-row p-5 space-x-3'>
+      <div className='flex items-center m-5 space-x-3'>
         <p className='text-3xl font-bold underline'>Hello World!</p>
-        <Button className='text-xl m-5 font-bold' variant="contained">Contained</Button>
+        <Button className='text-xl py-2 font-bold' variant="contained">Contained</Button>
       </div>
     </>
   )
